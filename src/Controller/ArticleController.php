a6f09df04ce8a214d69d78eda0ec7c39360150bb
kiamov <yunglist@mail.ru>
2019-08-31 19:50:18 +0300

    Let's gogit add .!

---
+++ src/Controller/ArticleController.php
@@ -0,0 +1,30 @@
+<?php
+
+
+namespace App\Controller;
+
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Response;
+
+class ArticleController
+{
+    /**
+     * @Route("/")
+     */
+    public function homepage()
+    {
+        return new Response('OMG! My first page already! Wooo!');
+    }
+
+    /**
+     * @Route("/news/{slug}")
+     */
+    public function show(string $slug)
+    {
+        return new Response(sprintf(
+            'Future page to show the article: "%s"',
+            $slug
+        ));
+    }
+}
